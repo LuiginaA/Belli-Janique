550d2f7287ff6a9f88c3c4f100813e3716fe9a8a
LuiginaA <luigina.argentin@gmail.com>
2024-11-01 18:24:45 +0100

    add animation on Hero section add contact section with map images and clinico add styling

---
+++ script.js
@@ -0,0 +1,9 @@
+// let navlist = document.querySelector('.navlist');
+
+// let menu = document.querySelector('menu-icon');
+// let navlist = document.querySelector('.navlist');
+
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x');
+//     navlist.classList.toggle('open');
+// }
