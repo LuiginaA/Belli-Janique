269b9eef6db97861a8cdd3075cf3b121f48c3b90
LuiginaA <luigina.argentin@gmail.com>
2025-01-18 16:19:48 +0100

    Add changes to media queries

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+// // Clinico Link 
+
+// <script type='text/javascript'>
+       
+// var clinikoBookings = document.getElementById('cliniko-49363333');
+// if (typeof e.data !== 'string') return;
+// if (e.data.search('cliniko-bookings-resize') > -1) {
+//     var height = Number(e.data.split(':')[1]);
+//     clinikoBookings.style.height = height + 'px';
+// }
+// e.data.search('cliniko-bookings-page') > -1 && clinikoBookings.scrollIntoView();
+// });
+// </script>
